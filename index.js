2ccf285a3f6d43ae1b7544562505873afbcf894c
Dev Isaac <isaaczik9@gmail.com>
2024-11-18 09:04:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import memes from 'memes.json';
+import memes from './memes.json';
 
 export default function handler(req, res) {
   // Allow requests from any origin
   res.setHeader('Access-Control-Allow-Origin', '*');
+  // Sets content to json
   res.setHeader('Content-Type', 'application/json');
+  // Returns a 200 HTTP message
   res.status(200).json(memes);
 }
